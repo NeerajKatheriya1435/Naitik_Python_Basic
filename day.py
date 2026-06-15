b978e2ace6bdbb32e68ea95de3c9b4e6e7c227dd
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-15 18:18:27 +0530

    strings methods

---
+++ day.py
@@ -0,0 +1,62 @@
+
+# name=input("Enter your name: ")
+
+# print("Nice to meet you",name)
+
+# num1=float(input("Enter the num1: "))
+# num2=float(input("Enter the num2: "))
+
+# print(num1*num2)
+
+# side = int(input("Enter the side: "))
+
+# print("Area of Square is:",side*side)
+# print("Permeter of Square is:",4*side)
+
+# Operators
+
+# print(7/3) # 2.
+# print(7//3) # 2
+# print(13%5) # 3
+# print(2**3) # 8
+
+# print(6>=6) # T
+# print(6>3) # T
+# print(3<=6) # T
+# print(8<6) # F
+# print(6==6) # T
+# print(6!=6) # F
+
+# a=7
+# # a=a+4
+# a+=9
+# a-=3
+# a*=3
+# a/=13
+# a**=3
+# a%=10
+# print(a)
+
+# print(5>17 and 5<9 and 4<=4) # F
+# print(15>7 and 5<19 and 4<=4) # T
+# print(5>7 or 15<9 or 4<=4) # T
+# print(1>7 or 15<9 or 14<=4) # F
+
+# print(not 7>8)
+# print(not 17>8)
+
+# name="Rohan kaise ho"
+# name="""
+# hello aniket how are you
+# Rohan kaise ho
+# main ghar ja raha hu
+# """
+# print(name)
+
+name="Naitik Kumar Singh"
+
+# print(name[0])
+# print(name[1])
+# print(name[2:5])
+print(name.upper())
+print(name.lower())
---
+++ day2.py
@@ -0,0 +1,57 @@
+# str1="Naitik dgfdgfdgbfdg"
+# print(str1)
+# str1="Ayush Kumar"
+# print(str1)
+# print(str1[0])
+# print(str1[1])
+
+# print(str1[6:9])
+# print(str1[6:])
+# print(str1[:6])
+# print(str1[::2])
+# print(str1[-1])
+# print(str1[-3])
+# print(str1[-3:])
+
+str1="Hello guys kaise ho kah ja rahe ho"
+# str1=input("Enter your name: ")
+
+# print(str1.upper())
+
+# print(str1.lower())
+# print(str1.rstrip("!"))
+# print(str1.replace("guys","student"))
+
+# str2 = "hello WorlD"
+# print(str2.capitalize())
+
+# str1 = "Welcome to the Console!!!"
+# print(str1.center(50))
+
+# name="Naitik Kumar"
+# print(name.count("a"))
+
+# str1 = "Welcome to the Console !!!"
+# # print(str1.endswith("!!!"))
+# print(str1.find("e"))
+
+# str1 = "WelcomeToTheConsole454"
+# print(str1.isalnum())
+
+# str1 = "Welcome56"
+# print(str1.isalpha())
+
+# str1 = "Hello world"
+# print(str1.islower())
+
+# str1 = "We wish you \n a Merry Christmas"
+# print(str1.isprintable())
+
+# str1 = "World Health Organization"
+# print(str1.istitle())
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.swapcase())
+
+str1 = "He's name is Dan. Dan is an honest man."
+print(str1.title())
