1a5c2d105998321fcffff6489a60093bf249195e
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-22 15:02:16 +0530

    set and dictionary

---
+++ day7.py
@@ -0,0 +1,71 @@
+
+# str1="Hello"
+# str2="Naitik"
+# str3="Raj"
+
+# name="Naitik"
+# age=34
+
+# print(str1,str2,str3)
+# print(str1,end=" ")
+# print(str2,end=" ")
+# print(str3)
+
+# str1="My name is: {1} and age is: {0}"
+# print(str1.format(name,age))
+# print(str1.format(age,name))
+# print(f"My name is: {name} and age is: {age}")
+
+# set1={3,67,5,7,3,2,6,7,7,1}
+
+# set1.add(90)
+# set1.add(90)
+# set1.discard(90)
+# set1.remove(67)
+# set1.pop()
+# set1.pop()
+# set1.pop()
+# set1.pop()
+
+# set2=set1.copy()
+# set2.update([4,7,3])
+# print(set2)
+# print(type(set1))
+
+# d1={
+#     "apple":"seb",
+#     "guawa":"amrood"
+# }
+d2={
+    "name":"Rohan",
+    "age":34,
+    "course":"python"
+}
+
+# print(d1)
+
+# print(d1["apple"])
+# print(d1["guawa"])
+
+# d2["name"]="Shubham"
+# d2["city"]="Lucknow"
+# d2.pop("name")
+# d2.popitem()
+# print(d2)
+
+# for item in d2:
+#     print(item)
+
+# for item in d2:
+#     print(d2[item])
+
+# for item in d2.keys():
+#     print(item)
+
+# for item in d2.values():
+#     print(item)
+
+# print(d2.get("name"))
+
+# Input 3 value from user and put in dictionary(name age gender)
+# Take tuple insert 5 values and delete 2 values using typecasting
