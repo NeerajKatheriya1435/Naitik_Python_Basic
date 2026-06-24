65b5cb5d996dfb57bd6df5099f021d85b017dc7d
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-24 15:10:49 +0530

    turtle library and some funs like recatngle and circle

---
+++ day9.py
@@ -0,0 +1,63 @@
+# import turtle
+
+# myturtle=turtle.Turtle()
+# myturtle=turtle.circle(200)
+# turtle.title("Simple Cursor Game")
+# myturtle.speed(1)
+
+# myturtle.forward(20)
+# myturtle.right(90)
+# myturtle.forward(20)
+# myturtle.left(90)
+# myturtle.forward(20)
+# myturtle.right(90)
+# myturtle.forward(20)
+# myturtle.left(90)
+# myturtle.forward(20)
+
+# for i in range(4):
+#     myturtle.forward(100)
+#     myturtle.right(90)
+
+# for i in range(36):
+
+#     turtle.pencolor("red")
+#     turtle.pensize(2)
+#     turtle.speed(0)
+#     turtle.left(10)
+#     myturtle=turtle.circle(50)
+
+# turtle.penup()
+# # turtle.distance(100)
+# turtle.goto(180,0)
+# turtle.pendown()
+
+# for i in range(36):
+
+#     turtle.pencolor("blue")
+#     turtle.pensize(2)
+#     turtle.speed(0)
+#     turtle.left(10)
+#     myturtle=turtle.circle(30)
+
+# screen=turtle.Screen()
+# screen.mainloop()
+
+
+# import turtle
+
+# screen = turtle.Screen()
+# screen.bgcolor("white")
+
+# t = turtle.Turtle()
+# t.speed(0)
+
+# colors = ["#FF5733", "#33FF57", "#3357FF", "#FF33A8", "#A833FF"]
+
+# for i in range(360):
+#     t.pensize(3)
+#     t.pencolor(colors[i % 5])
+#     t.circle(i * 0.5)
+#     t.left(50)
+
+# turtle.done()
---
+++ delete.py
@@ -0,0 +1,44 @@
+
+
+import turtle
+
+t = turtle.Turtle()
+t.color("red")
+t.speed(0)
+# t.forward(100)
+# t.right(90)
+# t.forward(200)
+# t.left(90)
+# t.backward(300)
+# t.left(270)
+# t.backward(250)
+# t.left(90)
+# t.penup()
+# t.forward(50)
+# t.circle(50)
+
+# for i in range(4):
+#     t.forward(100)
+#     t.right(90)
+
+
+# Square
+# for i in range(4):
+#     t.forward(100)
+#     t.left(90)
+
+# for i in range(36):
+#     t.circle(100)
+#     t.left(10)
+
+# Roof
+# t.left(45)
+# t.forward(70)
+
+# t.left(90)
+# t.forward(70)
+# t.goto(300,400)
+t.speed(1)
+t.forward(400)
+screen = turtle.Screen()
+screen.mainloop()
