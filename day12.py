3eb1a131df60eae24a761be6293f9bf35c0f7b0b
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-27 15:04:45 +0530

    file handling and random

---
+++ day12.py
@@ -0,0 +1,44 @@
+
+
+# meriFile=open("data.txt","r")
+
+# meriFile=open("natik.txt","rb")
+# meriFile=open("natik.txt","w")
+
+# meriFile=open("natik.txt","a")
+
+# meriFile.write("sathjbvjkdf likha jayega")
+
+
+# print(meriFile.read())
+# print(meriFile.readline())
+# print(meriFile.readline())
+# print(meriFile.readline())
+
+# f = open('data.txt', 'r')
+
+# while True:
+#     line = f.readline()
+#     if not line:
+#         break
+#     print(line)
+
+# f = open('myfile.txt', 'w')
+# lines = ['hello natik\n', 'my bachcha\n', 'meri teesri line\n']
+# f.writelines(lines)
+# f.close()
+
+# f = open('myfile.txt', 'w')
+# lines = ['line 1', 'line 2', 'line 3']
+
+# for line in lines:
+#     f.write(line + '\n')
+# f.close()
+
+# meriFile=open("data.txt","r")
+# meriFile.seek(6)
+# print(meriFile.tell())
+# print(meriFile.read())
+# f = open('myfile.txt', 'w')
+# f.write("hello guys")
+# f.truncate(5)
---
+++ day13.py
@@ -0,0 +1,9 @@
+# import random
+
+# print(int(random.random()*10))
+# print("Your otp valid for 5 minute:",random.randint(1000,9999))
+# list1=[3,5,6,73,83,14,2,4,5]
+
+# print(random.choice(list1))
+# random.shuffle(list1)
+# print(list1)
