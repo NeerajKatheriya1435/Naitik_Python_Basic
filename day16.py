7d58f4afaea6101322a20f479c03d858f269a717
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-30 18:08:54 +0530

    tkinter library python

---
+++ day16.py
@@ -0,0 +1,61 @@
+# class Employee:
+#     company="Tesla"
+#     def __init__(self,name,age,salary):
+#         self.name=name
+#         self.age=age
+#         self.salary=salary
+
+#     def naitik(self):
+#         print(f"My name is: {self.name}")
+#         print(f"My age is: {self.age}")
+#         print(f"My salary is: {self.salary}")
+
+# rohan=Employee("Rohan",45,23000)
+# rohan.company="Honda"
+# print(rohan.company)
+# rohan.naitik()
+
+# mohan=Employee("Mohan",90,22000)
+# print(mohan.company)
+# mohan.naitik()
+
+import time
+# print("Program Start....")
+# time.sleep(3)
+# print("Your Computer is hacked and finding your address")
+# time.sleep(3)
+# print("Hello Mister Naitik from Bareilly...")
+# time.sleep(3)
+# print("Your Computer has been Hacked")
+# print("Hacking Competed..........")
+# print(int((((time.time()/60)/60)/24)/365))
+
+# print(time.localtime())
+
+# local=time.localtime()
+# print("Formatted time is:",time.strftime("%Y-%m-%d %H:%M:%S",local))
+# start=time.time()
+# sum=0
+# for i in range(100000000):
+#     sum=sum+i
+# end=time.time()
+# print(sum)
+# print("Total time:",(end-start))
+
+# import tkinter as tk
+# import time
+# root=tk.Tk()
+
+# root.title("Naitik App")
+# root.geometry("400x300")
+
+# buttonMy1=tk.Button(root,text="Natik Sharma")
+# buttonMy1.pack(pady=20)
+
+# def on_click():
+#     buttonMy1.config(text="Good Morning Sir")
+
+# buttonMy2=tk.Button(root,text="Submit Now",command=on_click)
+# buttonMy2.pack(pady=20)
+
+# root.mainloop()
